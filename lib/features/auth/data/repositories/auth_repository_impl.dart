444678063a419f87d3c3e9907c1f886d8d334ec1
dainavipurav <dainavipurav@gmail.com>
2024-12-08 20:48:16 +0530

    ref : usecases added repository implementation added usecase impl added

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,46 @@
+import 'package:blog_app/core/error/exception.dart';
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+  AuthRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userId = await remoteDataSource.loginWithEmailPassword(
+        email: email,
+        password: password,
+      );
+
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userId = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:fpdart/fpdart.dart';
+
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+
+class UserSignUp implements Usecase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+
+  UserSignUp(this.authRepository);
+
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignUpParams({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
