17be83d01a9cc7c2cc0b7b8b94aa49645cd1ab74
dainavipurav <dainavipurav@gmail.com>
2024-12-08 20:15:20 +0530

    ref : exception class added and api call implementation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:blog_app/core/secrets/supabase_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final supabase = await Supabase.initialize(
+    url: SupabaseSecrets.supabaseUrl,
+    anonKey: SupabaseSecrets.supabaseAnonKey,
+  );
+
   runApp(const MainApp());
 }
 
